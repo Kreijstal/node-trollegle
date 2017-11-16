31adaf0c15179d75d42f6ee7d2279d28d66ee6b6
Rainb <elektrischrainbow@gmail.com>
2017-11-16 20:09:16 +0000

    somehow I am an idiot again

--- captchaDialog.js
+++ captchaDialog.js
@@ -1,30 +1,31 @@
 var request = require('request');
-function CaptchaDialog(site,id){
- this.site=site; 
- this.challenge=null;
- this.server = "http://www.google.com/recaptcha/api/";
- this.id=id;
- this.incorrect=false;
+
+function CaptchaDialog(site, id) {
+    this.site = site;
+    this.challenge = null;
+    this.server = "http://www.google.com/recaptcha/api/";
+    this.id = id;
+    this.incorrect = false;
 }
-CaptchaDialog.prototype.getImageUrl=function(){
+CaptchaDialog.prototype.getImageUrl = function() {
     console.log(this.challenge);
-if(this.challenge==null)throw new Error("There is no challenge!");
-return this.server + "image?c=" + this.challenge 
+    if (this.challenge == null) throw new Error("There is no challenge!");
+    return this.server + "image?c=" + this.challenge
 }
-CaptchaDialog.prototype.update=function() {
-        console.log("update invoked");
-        this.challenge = null;
-        var conn = {
-                url: (this.server + "challenge?k=" + this.site + "&ajax=1&cachestop=" + Math.random())
-            };
-        return new Promise(function(resolve,reject){
+CaptchaDialog.prototype.update = function() {
+    var that = this;
+    console.log("update invoked");
+    this.challenge = null;
+    var conn = {
+        url: (this.server + "challenge?k=" + this.site + "&ajax=1&cachestop=" + Math.random())
+    };
+    return new Promise(function(resolve, reject) {
         request(conn, function(err, res, body) {
-        var regexResult=body.match(/challenge\s*:\s*'([^']*?)'[\w\W]+?is_incorrect\s*:\s*([^,]+)/);
-        this.challenge=regexResult[1];
-        console.log(regexResult[1],"AAA");
-        this.is_incorrect=regexResult[2]==="true";
-        resolve();
-            })
-            })
-    }
+            var regexResult = body.match(/challenge\s*:\s*'([^']*?)'[\w\W]+?is_incorrect\s*:\s*([^,]+)/);
+            that.challenge = regexResult[1];
+            that.is_incorrect = regexResult[2] === "true";
+            resolve();
+        })
+    })
+}
 module.exports = CaptchaDialog;
